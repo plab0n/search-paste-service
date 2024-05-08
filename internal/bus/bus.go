11019528470686ac9d037c7a0c26fe1307b70265
plab0n <plaboniubat16@gmail.com>
2024-05-08 18:40:13 +0600

    publish and subscribe message

--- internal/bus/bus.go
+++ internal/bus/bus.go
@@ -1,27 +1,44 @@
 package bus
 
+import "errors"
+
 type MessageBus struct {
 	channels map[string]chan interface{}
 }
 
 type Bus interface {
-	Publish(topic string, message interface{}) bool
-	Subscribe(topic string)
+	Publish(topic string, message interface{}) error
+	Subscribe(topic string) (chan interface{}, error)
 }
 
-var bus MessageBus
+var bus *MessageBus
 
-func NewMessageBus() *MessageBus {
+func New() *MessageBus {
+	if bus != nil {
+		return bus
+	}
 	bus := &MessageBus{
 		channels: make(map[string]chan interface{}),
 	}
 	return bus
 }
 
-func (m *MessageBus) Publish(topic string, message interface{}) bool {
-	return false
+func (m *MessageBus) Publish(topic string, message interface{}) error {
+	ch := m.channels[topic]
+	if ch == nil {
+		return errors.New("No topic found!")
+
+	}
+	ch <- message
+	return nil
 }
 
-func (m *MessageBus) Subscribe(topic string) {
-
+func (m *MessageBus) Subscribe(topic string) (chan interface{}, error) {
+	ch := m.channels[topic]
+	if ch != nil {
+		return ch, nil
+	}
+	ch = make(chan interface{})
+	m.channels[topic] = ch
+	return ch, nil
 }
