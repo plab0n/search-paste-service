bbd17f1534c725255ed93b8c8dc3684f20faaa19
plab0n <plaboniubat16@gmail.com>
2024-05-09 16:35:36 +0600

    added paste processor

---
+++ internal/workers/paste_processor.go
@@ -0,0 +1,46 @@
+package workers
+
+import (
+	"github.com/plab0n/search-paste/internal/bus"
+	"github.com/plab0n/search-paste/internal/model"
+	"time"
+)
+
+type PasteProcessor struct {
+	Worker
+}
+
+var topic string
+
+func Init() {
+	topic = "paste.Created"
+	p := &PasteProcessor{}
+	p.Process()
+}
+
+func (p *PasteProcessor) Process() error {
+	b := bus.New()
+	ch, err := b.Subscribe(topic)
+
+	if err != nil {
+		return err
+	}
+	go func() {
+		for i := 0; ; i++ {
+			// Send data to the channel
+			message := <-ch
+			if paste, ok := message.(model.Paste); ok {
+				if isUrl(paste.Text) {
+					//Crawl the url
+				} else {
+					//Create embedding
+				}
+			}
+			time.Sleep(time.Second) // Simulate some delay
+		}
+	}()
+}
+
+func isUrl(text string) bool {
+	return false
+}
