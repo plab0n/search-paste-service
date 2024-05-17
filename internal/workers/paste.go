234f0592c1e7f3169e3eb742d4cb9c82616ea970
plab0n <plaboniubat16@gmail.com>
2024-05-18 01:31:17 +0600

    publishing message to the topic for crawling if url

---
+++ internal/workers/paste.go
@@ -0,0 +1,46 @@
+package workers
+
+import (
+	"github.com/plab0n/search-paste/internal/bus"
+	"github.com/plab0n/search-paste/internal/model"
+	"github.com/plab0n/search-paste/pkg/logger"
+	"net/url"
+)
+
+type Paste struct {
+	Worker
+}
+
+var topic string
+
+func Init() {
+	topic = "paste.Created"
+	p := &Paste{}
+	p.Start()
+}
+
+func (p *Paste) Start() error {
+	b := bus.New()
+	err := b.SubscribeWithHandler(topic, func(message interface{}) error {
+		if paste, ok := message.(model.Paste); ok {
+			if ok, _ := isUrl(paste.Text); ok {
+				err := b.Publish("paste.crawl", paste.Text)
+				if err != nil {
+					logger.Log.Error(err)
+				}
+			} else {
+				//Create embedding
+			}
+		}
+		return nil
+	})
+	return err
+}
+
+func isUrl(u string) (bool, error) {
+	parsedUrl, err := url.Parse(u)
+	if err != nil {
+		return false, err
+	}
+	return parsedUrl.Scheme == "https" && parsedUrl.Host != "", nil
+}
