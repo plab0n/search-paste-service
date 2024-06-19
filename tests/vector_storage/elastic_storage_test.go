0f845641a8b9a2ddd467496ac708661e80d55e38
pb <plabon.biswas@orbitax.com>
2024-06-19 19:09:15 +0600

    working on index creation

--- tests/vector_storage/elastic_storage_test.go
+++ tests/vector_storage/elastic_storage_test.go
@@ -1,13 +1,21 @@
 package vector_storage_test
 
 import (
+	"context"
 	"github.com/plab0n/search-paste/internal/vector_storage"
 	"testing"
 )
 
-func Test_ElasticConnection(t *testing.T) {
+func Test_CreateConnection(t *testing.T) {
 	_, err := vector_storage.NewElasticDb()
 	if err != nil {
 		t.Error(err)
 	}
+}
+func Test_CreateIndex(t *testing.T) {
+	es, err := vector_storage.NewElasticDb()
+	if err != nil {
+		t.Error(err)
+	}
+	ctx := context.Background()
 }
